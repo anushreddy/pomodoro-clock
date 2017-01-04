20c52ce615b65ce680705baa5e6a5c10b3b5b4b4
Anush Reddy <anushreddy04@gmail.com>
2017-01-03 21:02:12 -0600

    modified

--- js/index.js
+++ js/index.js
@@ -1,84 +1,101 @@
-var init_break = 5, init_session = 25;
-var secs = 60;
-var ses_mins = init_session;
-var break_mins = init_break;
+var ses_mins = 25;
+var break_mins = 5;
 
+$("#break_down").click(function() {
+  break_mins = parseInt($("#break").html());
+  if (break_mins != 1) {
+    break_mins -= 1;
+    $("#break").html(break_mins);
+  }
+});
 
-$("#break_down").click(function(){
-   --break_mins;
+$("#break_up").click(function() {
+  break_mins = parseInt($("#break").html());
+  ++break_mins;
   $("#break").text(break_mins);
 });
-$("#break_up").click(function(){
-   ++break_mins;
-  $("#break").text(break_mins);
+
+$("#session_down").click(function() {
+  ses_mins = parseInt($("#session").html());
+  if (ses_mins != 0) {
+    ses_mins -= 5;
+    $("#session").text(ses_mins);
+  }
 });
-$("#session_down").click(function(){
-   --ses_mins;
-  $("#session").text(ses_mins);
-});
-$("#session_up").click(function(){
-   ++ses_mins;
+
+$("#session_up").click(function() {
+  ses_mins = parseInt($("#session").html());
+  ses_mins += 5;
   $("#session").text(ses_mins);
 });
 
 /* #############################*/
-var interval,interval1;
-$("#start").click(function(){
-  $("#start").prop("disabled",true);
-  
-   interval = window.setInterval( timer, 1000 ); 
+var interval, interval1;
+var secs = 60;
+/* #############################*/
+$("#start").click(function() {
+  $("#start").prop("disabled", true);
+  ses_mins -= 1;
+
+  interval = window.setInterval(timer, 1000);
 });
-secs = 60;
-  ses_mins = ses_mins - 1;
-  break_mins = break_mins - 1;
 
-function timer(){
-    secs--;
-    console.log(secs);
-    $("#timer_mins").text(ses_mins);
-    $("#timer_secs").text(secs);
-    if(secs == 0 ){
-      secs = 60;
-      ses_mins -= 1;
-    }
-  if(ses_mins < 1){
-    window.clearInterval(interval);
+function timer() {
+  secs--;
+  $("#timer_mins").text(ses_mins);
+  $("#timer_secs").text(secs);
+  if (secs == 0) {
     secs = 60;
-     interval1 = window.setInterval(break_time,1000);
-  } 
-  };
-
-function break_time(){
-    secs--;
-    console.log(secs);
-  $("#session_name").text("Break Time");
-    $("#timer_mins").text(break_mins);
-    $("#timer_secs").text(secs);
-    if(secs == 0 ){
+    ses_mins -= 1;
+    if (ses_mins === 0) {
+      window.clearInterval(interval);
       secs = 60;
-      break_mins--;
+      break_mins -= 1;
+      ses_mins = parseInt($("#session").html());
+      $("#session_name").html("Break Time");
+      interval1 = window.setInterval(break_time, 1000);
     }
-    if(break_mins < 1){
+  }
+
+};
+
+function break_time() {
+  secs--;
+  $("#timer_mins").text(break_mins);
+  $("#timer_secs").text(secs);
+  if (secs == 0) {
+    secs = 60;
+    break_mins -= 1;
+    if (break_mins === 0) {
       window.clearInterval(interval1);
       secs = 60;
-    interval = window.setInterval( timer, 1000 );
+      ses_mins -= 1;
+      break_mins = parseInt($("#break").html());
+      $("#session_name").html("Session Time");
+      interval = window.setInterval(timer, 1000);
     }
-  };
+  }
 
+};
 
-$("#stop").click(function(){
+$("#stop").click(function() {
   window.clearInterval(interval);
   window.clearInterval(interval1);
-  $("#start").prop("disabled",false);
+  $("#start").prop("disabled", false);
 });
 
-$("#reset").click(function(){
-  $("#start").prop("disabled",false);
-  ses_mins = 25;
+$("#reset").click(function() {
+  $("#start").prop("disabled", false);
   window.clearInterval(interval);
   window.clearInterval(interval1);
+  ses_mins = 25;
+  break_mins = 5;
+  secs = 60;
   $("#timer_mins").html(ses_mins);
   $("#timer_secs").html("00");
   $("#session").text(ses_mins);
-  
+  $("#break").text(break_mins);
+
 });
+
+$("#fname").hide();
